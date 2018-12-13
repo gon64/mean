114b6bebc5a78edcb8fa5a9b5031a0269d2e1e32
Gon_zillA <gonzaloventura@outlook.com>
2018-12-13 18:37:33 +0100

    API

---
+++ server/models/employee.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const EmployeeSchema = new Schema({
+    name:       { type : String, required : true },
+    position:   { type : String, requires : true },
+    office:     { type : String, required : true },
+    salary:     { type : Number, required : true }
+})
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
